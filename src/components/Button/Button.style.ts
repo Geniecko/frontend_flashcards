4cfc31b715ca25e5002ec51c249b8a6f8ae265e3
Geniecko <radoslawkania09@gmail.com>
2022-09-12 16:37:56 +0200

    create button component

---
+++ src/components/Button/Button.style.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components/macro';
+
+export const Button = styled.button`
+  background-color: ${({theme}) => theme.primary};
+  color: ${({theme}) => theme.whitePrimary};
+  font-weight: 500;
+  padding: 8px 20px;
+  min-width: 100px;
+  cursor: pointer;
+  transition: .2s ease-in-out;
+
+  @media (hover: hover) {
+    &:hover{
+      opacity: .8;
+    }
+  }
+`;
