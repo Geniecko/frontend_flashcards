2e2df18354198f6544b6320e35f086a70e1143da
Geniecko <radoslawkania09@gmail.com>
2022-09-12 17:14:19 +0200

    change logo styles

--- src/components/Logo/Logo.style.ts
+++ src/components/Logo/Logo.style.ts
@@ -2,11 +2,15 @@ import styled from 'styled-components/macro';
 
 export const StyledLogo = styled.span`
   display: block;
-  font-size: 2.2rem;
-  color: ${({theme}) => theme.primary};
+  font-size: 2rem;
+  background-color: ${({theme}) => theme.primary};
+  color: ${({theme}) => theme.whitePrimary};
   font-weight: 600;
+  padding: 16px;
+  white-space: none;
 
   @media (min-width: 760px){
-    font-size: 2.4rem;
+    font-size: 2.2rem;
+    padding: 16px 24px;
   }
 `;
